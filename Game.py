5490b7b751fac9d7a1e978e078e003b9fff04aeb
carllindquist <clindqui@ucsc.edu>
2016-12-19 02:16:10 -0800

    Enhanced printRoom(): 	Changed print to automatically frame text based on linewidth 	Ignores newlines, so just type any description using “”” notation 	has a max line count of 10 for now, just for stylistic reasons, can be changed in printRoom() 	NOTE: Descriptions can no longer be set to ‘None’, for no text use “”
    info command now states the current structure

--- Game.py
+++ Game.py
@@ -44,7 +44,20 @@ class Structure(object):
 		return self.layout[self.r][self.c]
 
 	def printRoom(self):
-		lineWidth = 68
+		lineWidth = 69 #width of text box
+		words = self.curRoom().desc.split()
+		descLines = []
+		lNum = 0
+
+		for l in range(10): #max number of descriptions lines (limited for style)
+			descLines.append("")
+
+		for w in words:
+			if (len(descLines[lNum]) + len(w) + 1 <= lineWidth - 15):
+				descLines[lNum] += " "+w
+			else:
+				lNum += 1
+				descLines[lNum] += " "+w
 
 		r = self.r
 		c = self.c
@@ -62,16 +75,14 @@ class Structure(object):
 		if (self.curRoom().visited):
 			print " ----------- " + ("[%s]" % self.curRoom().name).center(lineWidth - 13)
 		else:
-			print " ----------- " + ("{%s}" % self.curRoom().name).center(lineWidth - 13)
-		if (self.curRoom().desc != None):
-			print "|    |%s|    |  " %up + self.curRoom().desc[0]
-			print "| |%s|   |%s| |  " %(left, right) + self.curRoom().desc[1]
-			print "|    |%s|    |  " %down + self.curRoom().desc[2]
-		else:
-			print "|    |%s|    |  " %up
-			print "| |%s|   |%s| |  " %(left, right)
-			print "|    |%s|    |  " %down
-		print " ----------- "
+			print " ----------- " + ("{{%s}}" % self.curRoom().name).center(lineWidth - 13)
+		print "|    |%s|    |  " %up + descLines[0]
+		print "| |%s|   |%s| |  " %(left, right) + descLines[1]
+		print "|    |%s|    |  " %down + descLines[2]
+		print " -----------   " + descLines[3]
+		for l in descLines[4:]:
+			if(l):
+				print " "*15 + l
 		print "-" * lineWidth
 
 	def setLocation(self, row, column):
@@ -120,39 +131,33 @@ building = [[None for x in range(5)] for y in range(5)]
 
 sam = Character("Sam", "  Hey brother")
 
-r = Room("Bedroom", [
-		 "Two beds side by side.",
-		 "",
-		 ""],
-		 None, sam)
+building[2][2] = Room("Bedroom", """
+	Two beds, side by side. Light shines in through two windows
+	in front of you.
+""", None, sam)
 
-building[2][2] = r
-
-building[0][3] = Room("Hallway", None, None, None)
-building[1][3] = Room("Hallway", None, None, None)
-building[2][3] = Room("Hallway", None, None, None)
-building[3][3] = Room("Hallway", None, None, None)
+building[0][3] = Room("Hallway", "", None, None)
+building[1][3] = Room("Hallway", "", None, None)
+building[2][3] = Room("Hallway", "", None, None)
+building[3][3] = Room("Hallway", "", None, None)
 
 ginger = Character("Ginger", "  Rrraow")
 
-building[4][3] = Room("Living Room", [
- 					  "A grand room with a fireplace.",
- 					  "",
- 					  ""],
- 					  ["Ripstick"],
- 					   ginger)
+building[4][3] = Room("Living Room", """
+	A grand old room with a fireplace. Large
+	wooden beams cross over your head. A piano stands by 
+	a window. The fireplace looks used...recently.
+""", ["Ripstick"], ginger)
 
-building[1][4] = Room("Kitchen", [
- 					  "A small kitchen with blue tile countertops.",
- 					  "",
- 					  ""],
- 					  ["Matches"],
- 					   None)
+building[1][4] = Room("Kitchen", """
+	A small kitchen with blue tile countertops.
+""",
+	["Matches"], None)
 
 inventory = []
 
 
-home = Structure("House", "3516", building)
+home = Structure("The House", "3516", building)
 
 home.setLocation(2, 2)
 
@@ -181,7 +186,8 @@ while True:
 			home.attemptMove(input)
 			break
 		elif (input == 'info' ):
-			print "Currently in a %s." % cRoom.name
+			print "Currently in %s." % home.name
+			print home.desc
 		else:
 			print "Pick another way: "
 
